5b6ad42b3e55631606d5270754009c551fa45b2e
Alvin S. Raquem <41246806+AlvinRaquem@users.noreply.github.com>
2019-05-21 20:35:03 +0800

    Update index.php

--- Web app/HelpdeskEvent/index.php
+++ Web app/HelpdeskEvent/index.php
@@ -8,7 +8,4 @@ include "autoload.php";
 include "apiroutes.php";
 include "routes.php";
 
-$request = isset($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : '';
-$userlevel = isset($_SESSION['USER_LEVEL']) ? $_SESSION['USER_LEVEL'] : 'GUEST';
-
 

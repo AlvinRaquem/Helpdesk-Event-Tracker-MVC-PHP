bc5d50ce9872162c80f7df1526b7f4059a0edd44
Alvin S. Raquem <41246806+asraquem00110@users.noreply.github.com>
2020-08-12 09:32:54 +0800

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -13,32 +13,11 @@
 // });
 
 // for linux
-spl_autoload_register(function($classname){
+spl_autoload_register(function($class){
 		
-	$file =  APP_PATH.strtr($classname, "\\", "/") . ".php";
-	
-	$file = str_replace("//","/",$file);
-	
-	if(file_exists($file)){
+		$file = str_replace('\\','/',APP_PATH.$class.'.php');
+
+		if(file_exists($file)){
 		require $file;
 	}
-	
-	$file2 =  BASE_PATH.strtr($classname, "\\", "/") . ".php";
-	
-	$file2 = str_replace("//","/",$file2);
-	
-	if(file_exists($file2)){
-		require $file2;
-	}
-	
-	$file3 =  strtr($classname, "\\", "/") . ".php";
-	
-	$file3 = str_replace("//","/",$file3);
-	
-	if(file_exists($file3)){
-		require $file3;
-	}
-	
-
 });
-

1020d3c02ba32af3aedfc4443d869a44556fd63b
Alvin S. Raquem <41246806+asraquem00110@users.noreply.github.com>
2020-08-12 09:44:23 +0800

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,23 +1,23 @@
 <?php
 
-// // for windows
-// spl_autoload_register(function($class){
-
-// 	$file = APP_PATH.$class.".php";
-
-// 	if(file_exists($file))
-// 		require $file;
-// 	// }else if(file_exists(BASE_PATH.$class.".php")){
-// 	// 	require BASE_PATH.$class.".php";
-// 	// }
-// });
-
-// for linux
+// for windows
 spl_autoload_register(function($class){
-		
-		$file = str_replace('\\','/',APP_PATH.$class.'.php');
 
-		if(file_exists($file)){
+	$file = APP_PATH.$class.".php";
+
+	if(file_exists($file))
 		require $file;
-	}
+	// }else if(file_exists(BASE_PATH.$class.".php")){
+	// 	require BASE_PATH.$class.".php";
+	// }
 });
+
+// // for linux
+// spl_autoload_register(function($class){
+		
+// 		$file = str_replace('\\','/',APP_PATH.$class.'.php');
+
+// 		if(file_exists($file)){
+// 		require $file;
+// 	}
+// });
